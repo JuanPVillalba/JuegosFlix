61b0c1d8f3996bbf2b85355a0a16e5b2dfb63e82
ralepromotions <ralepromotions@gmail.com>
2023-10-17 16:25:49 -0300

    sección acción,imágenes,css y js básico

---
+++ js/script.js
@@ -0,0 +1,5 @@
+function prevButton() { window.location.href = './index.html'; }
+function nextButton() { window.location.href = './index.html'; }
+function homeButton() { window.location.href = './index.html'; }
+function accionButton() { window.location.href = './accion.html'; }
+function subirButton() { window.scrollTo({top: 0}) }
