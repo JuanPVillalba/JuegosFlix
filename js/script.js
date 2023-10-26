0b7beb889145c71f13f1881685e2c20395044a3e
juan villalba <juanpaulolp@gmail.com>
2023-10-26 17:26:10 -0300

    AGREGADO DE PAGUINA AVENTURA

--- js/script.js
+++ js/script.js
@@ -2,4 +2,5 @@ function prevButton() { window.location.href = './index.html'; }
 function nextButton() { window.location.href = './index.html'; }
 function homeButton() { window.location.href = './index.html'; }
 function accionButton() { window.location.href = './accion.html'; }
+function aventuraButton() { window.location.href = './aventura.html'; }
 function subirButton() { window.scrollTo({top: 0}) }
