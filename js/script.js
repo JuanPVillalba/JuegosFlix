9e3a80af59001ef3cb26b971918134697140690a
Marcelo Celano <104109443+MAC032@users.noreply.github.com>
2023-10-27 11:08:42 -0300

    Links y organización de archivos

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
-function prevButton() { window.location.href = './index.html'; }
-function nextButton() { window.location.href = './index.html'; }
+function prevButton() { window.location.href = '../index.html'; }
+function nextButton() { window.location.href = '../index.html'; }
 function homeButton() { window.location.href = './index.html'; }
 function accionButton() { window.location.href = './accion.html'; }
+function clasicosButton() { window.location.href = './pages/clasicos.html'; }
 function aventuraButton() { window.location.href = './aventura.html'; }
 function subirButton() { window.scrollTo({top: 0}) }
